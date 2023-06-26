f205095ce0f533399434de72fffb2ae0eb12aa1b
Chieh Chang <raphaelpeo@gmail.com>
2023-06-26 19:15:11 +0800

    Update utility.py

--- script/utility.py
+++ script/utility.py
@@ -65,8 +65,6 @@ def calc_chebynet_gso(gso):
     id = sp.identity(gso.shape[0], format='csc')
     # If you encounter a NotImplementedError, please update your scipy version to 1.10.1 or later.
     eigval_max = norm(gso, 2)
-    # If unable to upgrade scipy, use the alternative code below.
-    #eigval_max = norm(gso, 'fro')
 
     # If the gso is symmetric or random walk normalized Laplacian,
     # then the maximum eigenvalue is smaller than or equals to 2.
